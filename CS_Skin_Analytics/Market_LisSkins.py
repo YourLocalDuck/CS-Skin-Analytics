69a15e615c0c90d5279b919f6e5edeac977084b0
YourLocalDuck <prince@LAPTOP-HNH3RBSN.localdomain>
2023-11-15 13:53:04 -0500

    attempt at selenium

--- CS_Skin_Analytics/Market_LisSkins.py
+++ CS_Skin_Analytics/Market_LisSkins.py
@@ -1,17 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium_stealth import stealth
 
 # No API documentation, reverse engineering not possible. Probably use BeatifulSoup to scrape the website.
 class Market_LisSkins():
     def __init__(self):
-        super().__init__()
-        self.name = "LisSkins"
         self.url = "https://lis-skins.ru/market/cs2/"
+        options = webdriver.ChromeOptions()
+        service = webdriver.ChromeService(executable_path='./home/prince/chrome-linux64/chrome')
+        options.add_argument("--headless")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+        self.driver = webdriver.Chrome(service=service, options=options)
         self.soup = self.get_soup()
+        print(self.soup)
         
     def get_soup(self):
-        response = requests.get(self.url)
-        soup = BeautifulSoup(response.content, 'html.parser')
+        #response = requests.get(self.url)
+        self.driver.get(self.url)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
         return soup
     
     def get_items(self):
