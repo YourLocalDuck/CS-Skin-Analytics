a930554d3703c56b7d7b123e93545f29c5d642bb
Prince <prince@DESKTOP-RN5954U>
2023-12-24 05:27:37 -0500

    Main loop sell/buy constraint. Output CSV implemented

--- CS_Skin_Analytics/CS_Skin_Analytics.py
+++ CS_Skin_Analytics/CS_Skin_Analytics.py
@@ -3,6 +3,7 @@ from Market_Buff import Buff
 from Market_Skinport import Skinport
 from Market_Steam import Steam
 from Market_Bitskins import Bitskins
+import csv
 
 
 def readSkinNames():
@@ -28,12 +29,16 @@ def getSettings():
 
 appSettings = getSettings()
 buff = Buff(appSettings['Cookie'])
-buff.readFromFile()
+buff.initializeMarketData()
+buff.writeToFile()
+#buff.readFromFile()
 #skinport = Skinport()
 #steam = Steam()
 #bitskins = Bitskins()
 skinout = Skinout()
-skinout.readFromFile()
+skinout.initializeMarketData()
+skinout.writeToFile()
+#skinout.readFromFile()
 #steam.readFromFile()
 
 Markets = [ skinout, buff ]
@@ -66,10 +71,27 @@ for skinName in skinsList:
     if skinPrice:
         maxPrice = max(skinPrice, key=lambda x:x['price'])
         minPrice = min(skinPrice, key=lambda x:x['price'])
-        profitSummary.append({"Name": skinName,"Profit": (maxPrice['price']-minPrice['price']),"Buy Market": minPrice['market'].__name__, "Buy Price": minPrice['price'], "Sell Market": maxPrice['market'].__name__, "Sell Price": maxPrice['price']})
+        profitSummary.append({"Name": skinName,"Relative Profit": (maxPrice['price']/minPrice['price']), "Profit": (maxPrice['price'] - minPrice['price']), "Buy Market": minPrice['market'].__name__, "Buy Price": minPrice['price'], "Sell Market": maxPrice['market'].__name__, "Sell Price": maxPrice['price']})
         
-sortedProfitSummary = sorted(profitSummary, key=lambda x: x["Profit"], reverse=True)
+sortedProfitSummary = sorted(profitSummary, key=lambda x: x["Relative Profit"], reverse=True)
 
 for i in sortedProfitSummary:
     if (i["Profit"] > 0):
         print(i)
+        
+# Generate a CSV file with the following columns: Name, Relative Profit, Profit, Buy Market, Buy Price, Sell Market, Sell Price.
+# Sort the rows by Relative Profit in descending order.
+# Only include rows where Profit > 0 and Buy Market != "Buff"
+
+# Assuming sortedProfitSummary is the list containing the profit summary data
+
+filteredSummary = [row for row in sortedProfitSummary if row["Profit"] > 0 and row["Buy Market"] != "Buff"]
+
+csv_file = "profit_summary.csv"
+
+with open(csv_file, mode='w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=["Name", "Relative Profit", "Profit", "Buy Market", "Buy Price", "Sell Market", "Sell Price"])
+    writer.writeheader()
+    writer.writerows(filteredSummary)
+
+sortedSummary = sorted(filteredSummary, key=lambda x: x["Relative Profit"], reverse=True)
