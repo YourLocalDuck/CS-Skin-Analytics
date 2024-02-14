87004c2f75e7a29b1ab2c4cb1bda2fbef67e3911
MillerX01 <54687132+MillerX01@users.noreply.github.com>
2024-02-14 17:36:43 -0500

    Probably breaks something, maybe the right idea.

--- CS_Skin_Analytics/GenerateAnalysis.py
+++ CS_Skin_Analytics/GenerateAnalysis.py
@@ -2,31 +2,37 @@ from typing import List
 import pandas as pd
 from Market_Base import Market_Base
 
+
 class Generate_Analysis:
     def __init__(self, buy_markets: List[Market_Base], sell_markets: List[Market_Base]):
         self.buy_markets = buy_markets
         self.sell_markets = sell_markets
-        
+        self.buyMarketItemData = []
+        self.sellMarketItemData = []
+
     # Read from Skin_names.txt for the skins names to be analyzed
-    def readSkinNames():
+    def readSkinNames(self):
         skinList = []
         with open("skins_names.txt", "r", encoding="utf-8") as file:
             for line in file:
                 skinList.append(line.strip())
         return skinList
-    
+
     def getData(self):
-        buyMarketItemData = []
-        sellMarketItemData = []
+
         for market in self.buy_markets:
-            buyMarketItemData.append(market.formatData())
+            self.buyMarketItemData.append(market.getFilteredData())
+        print(self.buyMarketItemData)
         for market in self.sell_markets:
-            sellMarketItemData.append(market.formatData())
-                
-        
+            self.sellMarketItemData.append(market.getFilteredData())
+        print(self.sellMarketItemData)
+
     def sortData(self):
-        pass
-    
+        buyMarket = pd.DataFrame(self.buyMarketItemData)
+        sellMarket = pd.DataFrame(self.sellMarketItemData)
+        mergedMarket = pd.merge(buyMarket, sellMarket, on="Item", suffixes=("_B", "_S"))
+        return mergedMarket
+
     def generateAnalysis(self, itemname):
         price = self.market.getPrice(itemname)
         sale_price = self.market.getSalePrice(itemname)
@@ -39,4 +45,3 @@ class Generate_Analysis:
                 "Unlock Time": [unlock_time],
             }
         )
-        
